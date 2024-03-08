a40bf5818b7c92b353b98a835a840ad012fea33b
tomobre <tomobre@gmail.com>
2024-03-08 16:37:40 +0100

    trying solve  backedn error

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,6 +1,6 @@
 import db from "../../config/db";
 
-export async function POST(request) {
+export async function POST(request, res) {
   const userData = await request.json();
   const {
     pelicula,
@@ -33,12 +33,12 @@ export async function POST(request) {
       `INSERT INTO OscarPredicciones (NombreParticipante,pelicula,direccion,guionAdap, guionOri, montaje,sonido, internacional, actor,actriz,actorRep,actrizRep,foto,vestuario,efectos,disProd,animacion,docu,cortoAnim,cortoDoc,corto,bandaSonora,cancion,maqYPel) VALUES ('${userData.username}','${pelicula}','${direccion}','${guionAdap}', '${guionOri}', '${montaje}','${sonido}','${internacional}', '${actor}','${actriz}','${actorRep}','${actrizRep}','${foto}','${vestuario}','${efectos}','${disProd}','${animacion}','${docu}','${cortoAnim}','${cortoDoc}','${corto}','${bandaSonora}','${cancion}','${maqYPel}');`,
       (err, results) => {
         if (err) {
-          reject(err);
+          return res.json(err);
         } else {
           resolve(results);
         }
       }
     );
   });
-  return Response.json({ results, err });
+  //return Response.json({ results, err });
 }
