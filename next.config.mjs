4ec4c5c7afe4c3682875af5bcdf2fb3bb61a2ca8
tomobre <tomobre@gmail.com>
2024-03-08 12:41:14 +0100

    removd unnevesary code from next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,55 +1,7 @@
 /** @type {import('next').NextConfig} */
-import TerserPlugin from "terser-webpack-plugin";
+//import TerserPlugin from "terser-webpack-plugin";
 const nextConfig = {
   output: "export",
-  webpack(config) {
-    // Remove TerserPlugin if not needed for minification
-    if (!config.optimization) {
-      config.optimization = {};
-    }
-    if (!config.optimization.minimizer) {
-      config.optimization.minimizer = [];
-    }
-
-    // Add TerserPlugin with optimized options
-    config.optimization.minimizer.push(
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false,
-        },
-        // Limit parallelism to reduce memory usage
-        parallel: false,
-      })
-    );
-
-    config.optimization.minimize = false;
-
-    return config;
-  },
-  webpack(config) {
-    // Remove TerserPlugin if not needed for minification
-    if (!config.optimization) {
-      config.optimization = {};
-    }
-    if (!config.optimization.minimizer) {
-      config.optimization.minimizer = [];
-    }
-
-    // Add TerserPlugin with optimized options
-    config.optimization.minimizer.push(
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false,
-        },
-        // Limit parallelism to reduce memory usage
-        parallel: false,
-      })
-    );
-
-    config.optimization.minimize = false;
-
-    return config;
-  },
 };
 
 export default nextConfig;
