793a4e2e5e419617bdac53054ac1fbf26d425eff
tomobre <tomobre@gmail.com>
2024-03-08 13:32:30 +0100

    tailwind try working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
